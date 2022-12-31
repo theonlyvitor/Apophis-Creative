66ad9b343b01cf1a0f63891a4aeee9a5be5149a5
Vitor Hugo <vitorhrm24@gmail.com>
2022-12-30 21:38:05 -0400

    Deploy WebSite

---
+++ js/titleAnimation.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function typeWriter(el) {
+    const textArray = el.innerHTML.split('');
+    el.innerHTML = '';
+    textArray.forEach((letter, i) =>
+        setTimeout(() => (el.innerHTML += letter), 95 * i)
+    );
+
+    setInterval(() => typeWriter(el), 8000);
+}
+
+typeWriter(elementEl);
